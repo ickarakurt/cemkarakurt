058b6efffc57874a2c11d453647c32bac90ac66a
Cem Karakurt <ickarakurt@gmail.com>
2026-04-05 15:16:43 +0200

    fix: TOC css change

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -5,18 +5,25 @@ import { slugifyStr } from "./slugify";
 interface Tag {
   tag: string;
   tagName: string;
+  count: number;
 }
 
 const getUniqueTags = (posts: CollectionEntry<"blog" | "note">[]) => {
-  const tags: Tag[] = posts
-    .filter(contentFilter)
-    .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
-    .filter(
-      (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
-    )
-    .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
+  const allTags = posts.filter(contentFilter).flatMap(post => post.data.tags);
+  const tagCounts = allTags.reduce((acc, tag) => {
+    const slug = slugifyStr(tag);
+    acc[slug] = (acc[slug] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  const tags: Tag[] = Object.keys(tagCounts)
+    .map(slug => ({
+      tag: slug,
+      tagName: allTags.find(t => slugifyStr(t) === slug) || slug,
+      count: tagCounts[slug],
+    }))
+    .sort((tagA, tagB) => tagB.count - tagA.count || tagA.tag.localeCompare(tagB.tag));
+
   return tags;
 };
 
